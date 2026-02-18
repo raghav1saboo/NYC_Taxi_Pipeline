e70be2cef198a253cd0a7feb0556bc202a6cdcde
EC2 Default User <ec2-user@ip-172-31-46-248.ap-south-1.compute.internal>
2026-02-18 12:18:02 +0000

    added glue and python version to ingesting dataset task in airflow dag

--- dags/nyc_taxi_dag.py
+++ dags/nyc_taxi_dag.py
@@ -58,6 +58,8 @@ with DAG(
         job_name='nyc_taxi_parallel_ingest_job',
 	iam_role_name='DataEngineeringConductorRole',
         script_location='s3://raghav-saboo-ecommerce-lakehouse/scripts/industrial_parallel_ingest.py',
+	glue_version='4.0', 
+        python_version='3',
         script_args={
             '--BUCKET_NAME': 'raghav-saboo-ecommerce-lakehouse',
             # Logic: Use manual config if provided, otherwise use current execution year
